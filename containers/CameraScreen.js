8cc78bdadc4070867a7e066eaf0aff136ff77952
clecazzz <70854934+clecazzz@users.noreply.github.com>
2021-03-02 17:48:00 +0100

    SCAN et ENVOI VERS FICHE PRODUIT

--- containers/CameraScreen.js
+++ containers/CameraScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text, Button, SafeAreaView } from "react-native";
+import { View, Text, Button, SafeAreaView, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/core";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { useEffect, useState } from "react";
 
 // Envoi données produit scanné vers app.js
 
@@ -8,16 +10,68 @@ import { useNavigation } from "@react-navigation/core";
 
 const CameraScreen = () => {
     const nav = useNavigation();
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        // console.log(data);
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        setData(data);
+        console.log(data);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
 
     return (
-        <SafeAreaView>
-            <Button
-                title="Go to Produit"
-                onPress={() => {
-                    nav.navigate("Product");
+        <>
+            <View
+                style={{
+                    flex: 1,
+                    flexDirection: "column",
+                    justifyContent: "flex-end",
                 }}
-            />
-        </SafeAreaView>
+            >
+                <BarCodeScanner
+                    onBarCodeScanned={
+                        scanned ? undefined : handleBarCodeScanned
+                    }
+                    style={StyleSheet.absoluteFillObject}
+                />
+
+                <Button
+                    title="Go to Produit"
+                    onPress={() => {
+                        nav.navigate("Product", { data: data });
+                    }}
+                />
+
+                {scanned && (
+                    <Button
+                        title={"Tap to Scan Again"}
+                        onPress={() => setScanned(false)}
+                    />
+                )}
+            </View>
+            <Text style={styles.text}>{data}</Text>
+        </>
     );
 };
 export default CameraScreen;
+
+const styles = StyleSheet.create({
+    text: { fontSize: 34 },
+});
--- containers/ProductScreen.js
+++ containers/ProductScreen.js
@@ -1,11 +1,61 @@
 import React from "react";
-import { View, Text, Button, SafeAreaView } from "react-native";
+import {
+    View,
+    Text,
+    Button,
+    SafeAreaView,
+    Image,
+    StyleSheet,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { useRoute } from "@react-navigation/core";
+
+const axios = require("axios");
 
 // Reçoit donnée du produit
 
 // Possibilité d'ajouter en favoris -> envoi
 
-const ProductScreen = ({ navigation }) => {
-    return <SafeAreaView></SafeAreaView>;
+const ProductScreen = () => {
+    const [name, setName] = useState();
+    const [picture, setPicture] = useState();
+    const [brand, setBrand] = useState();
+
+    const { params } = useRoute();
+    // const id = route.params.data;
+    // console.log(params);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://world.openfoodfacts.org/api/v0/product/${params.data}`
+                );
+                console.log(response.data);
+                setName(response.data.product.product_name);
+                setPicture(response.data.product.image_front_small_url);
+                setBrand(response.data.product.brands);
+            } catch (error) {
+                console.log(error.response);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <Text>data : {params.data}</Text>
+
+            <View>
+                <Image source={{ uri: picture }} style={styles.productImage} />
+                <Text>{name}</Text>
+                <Text>{brand}</Text>
+            </View>
+        </SafeAreaView>
+    );
 };
 export default ProductScreen;
+
+const styles = StyleSheet.create({
+    productImage: { height: 160, width: 100 },
+});
